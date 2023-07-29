a34a0e4e2487be7900b2ee66af668a1646c9d6e2
Kaspar <kaspar.olman97gmail.com>
2023-07-29 09:41:59 +0300

    New variables + game logic

--- Blackjack game/index.js
+++ Blackjack game/index.js
@@ -1,11 +1,18 @@
 let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum < 21) {
+
+if (sum <= 20) {
     console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-    console.log("Blackjack!")
+} else if (sum===21) {
+    console.log("Blackjack!!!")
+    hasBlackJack = true
 } else {
     console.log("Busted!")
+    let isAlive = false
 }
+
